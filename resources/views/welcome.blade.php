1753e8c589dabe2f0a911cb6b15a759e64686b97
stephenhartman <stephen.c.hartman@gmail.com>
2018-01-17 13:04:53 -0500

    Add sessions, change layouts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,23 +9,14 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >p
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
                 <div class="title m-b-md">
-                    Pride AC Jacksonville
+                    <a href="{{ route('home') }}">Pride AC Jacksonville</a>
                 </div>
             </div>
         </div>
