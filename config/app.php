9ffd0aeb289b9e86b2f8130f5b487aec4b5f1535
stephenhartman <stephen.c.hartman@gmail.com>
2018-01-17 13:05:21 -0500

    Add composer updates, routes, config

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Pride AC Jacksonville'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'America/New_York',
 
     /*
     |--------------------------------------------------------------------------
@@ -164,6 +164,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+
         /*
          * Package Service Providers...
          */
@@ -177,6 +178,15 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Laravel Collective
+         */
+        Collective\Html\HtmlServiceProvider::class,
+
+        /*
+         * Laravel Phone
+         */
+        Propaganistas\LaravelPhone\PhoneServiceProvider::class
     ],
 
     /*
@@ -225,6 +235,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,14 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::middleware(['admin'])->group(function () {
+    Route::resource('/products/goodman', 'GoodmanController', ['except' => 'index', 'show']);
+});
+
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/products', 'ProductController@index')->name('products');
+Route::resource('products/goodman', 'GoodmanController', ['only' => 'index', 'show']);
+Route::get('about', 'AboutController@index')->name('about');
+
+Route::get('contact', 'ContactController@create')->name('contact.create');
+Route::post('contact', 'ContactController@store')->name('contact.store');
