dab1eef0263c6028a61913a0f145a06d8274a6e2
stephenhartman <stephen.c.hartman@gmail.com>
2018-01-17 13:05:39 -0500

    Add contact form and mail

---
+++ resources/views/contact/create.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Contact Pride AC Jacksonville</div>
+
+                    <div class="panel-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                            {{ Form::open(['route' => 'contact.store']) }}
+
+                            <div class="form-group">
+                                {{ Form::label('name', 'Your Name') }}
+                                {{ Form::text('name', null, ['class' => 'form-control']) }}
+                            </div>
+
+                            <div class="form-group">
+                                {{ Form::label('email', 'E-mail Address') }}
+                                {{ Form::text('email', 'example@gmail.com', ['class' => 'form-control']) }}
+                            </div>
+
+                            <div class="form-group">
+                                {{ Form::label('phone', 'Phone Number (Format: 123456789)') }}
+                                {{ Form::tel('phone', null, ['class' => 'form-control']) }}
+                            </div>
+
+                            <div class="form-group">
+                                {{ Form::label('sq_footage', 'Square Footage of Home') }}
+                                {{ Form::number('sq_footage', null, ['class' => 'form-control']) }}
+                            </div>
+
+                            <div class="form-group">
+                                {!! Form::textarea('notes', null, ['class' => 'form-control']) !!}
+                            </div>
+
+                            {!! Form::submit('Submit', ['class' => 'btn btn-info']) !!}
+
+                            {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,28 @@
+@component('mail::message')
+
+    <p>
+        You received a message from {{ $name }}
+    </p>
+    <p>
+        Name: {{ $name }}
+    </p>
+
+    <p>
+        Email: {{ $email }}
+    </p>
+
+    <p>
+        Phone Number: {{ $phone }}
+    </p>
+
+    <p>
+        Square Footage of Home: {{ $sq_footage }}
+    </p>
+
+    <p>
+        Additional Notes: {{ $notes }}
+    </p>
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
