b38580b17595ea2dd2b6e49799a4caaa6713fb6e
stephenhartman <stephen.c.hartman@gmail.com>
2018-01-17 21:44:25 -0500

    Add About

---
+++ resources/views/about/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">About Us</div>
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="text-center">
+                                <h2>More coming soon!</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
