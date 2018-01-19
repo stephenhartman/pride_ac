5d5cd8ece4f98ff1af7da9ba00d30e6dc9d5cc07
stephenhartman <stephen.c.hartman@gmail.com>
2018-01-18 22:55:38 -0500

    Update composer, assets, and mail config

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,23 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'no-reply@prideacjax.com'),
+        'name' => env('MAIL_FROM_NAME', 'Pride AC Jacksonville'),
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Global "To" Address
+    |--------------------------------------------------------------------------
+    |
+    | You may wish for all e-mails sent by your application to be sent to
+    | the same address. Here, you may specify a name and address that is
+    | used globally for all e-mails that are sent by your application.
+    |
+    */
+    'to' => [
+        'address' => env('MAIL_USERNAME'),
+        'name' => 'Pride AC Jacksonville'
     ],
 
     /*
