ac5b75c136da2af534a915de883fd46bf00dcf98
stephenhartman <stephen.c.hartman@gmail.com>
2018-01-19 14:22:03 -0500

    Add alert message to template

--- resources/views/layouts/messages.blade.php
+++ resources/views/layouts/messages.blade.php
@@ -8,14 +8,23 @@
 
         @if(Session::has('success'))
             <div class="alert alert-success" role="alert">
-                <strong>Success!</strong> {{Session::get('success')}}
+                <strong>Success!</strong>
+                {{Session::get('success')}}
             </div>
 
         @endif
 
         @if(Session::has('error'))
             <div class="alert alert-danger" role="alert">
-                <strong>Error!</strong> {{Session::get('error')}}
+                <strong>Error!</strong>
+                {{Session::get('error')}}
+            </div>
+        @endif
+
+        @if(Session::has('alert'))
+            <div class="alert alert-danger" role="alert">
+                <strong>Alert!</strong>
+                {{Session::get('alert')}}
             </div>
         @endif
 
