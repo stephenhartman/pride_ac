fadc52179301643755815a923b922db713f89480
stephenhartman <stephen.c.hartman@gmail.com>
2018-01-20 14:54:09 -0500

    Add warranty info for Goodman and Trane

--- resources/views/products/goodman/index.blade.php
+++ resources/views/products/goodman/index.blade.php
@@ -67,6 +67,35 @@
                         </table>
                     </div>
                 </div>
+                <div class="row">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4>Warranty Information</h4>
+                        </div>
+                        <div class="panel-body">
+                            <ul>
+                                <li>
+                                    Pride AC and Heating will warranty all labor for a period of one year.
+                                </li>
+                                <li>Goodman brand limited warranties cover defects in materials and
+                                    workmanship that appear under normal use and maintenance for a period of ten years.
+                                </li>
+                                <li>Product registration through Goodman entitles the owner of the unit to additional
+                                    rights and remedies where applicable.
+                                </li>
+                                <li>
+                                    Select higher efficiency Goodman brand air conditioners and heat pumps offer Lifetime
+                                    Compressor Limited Warranty to the original, registered owner. If the original registered
+                                    owner should have a compressor failure, we will replace the unit with a comparable Goodman
+                                    brand air conditioner or heat pump.
+                                </li>
+                                <li>
+                                    <a href="https://www.goodmanmfg.com/product-registration">You can register your Goodman Product through this link.</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/products/trane/index.blade.php
+++ resources/views/products/trane/index.blade.php
@@ -67,6 +67,36 @@
                         </table>
                     </div>
                 </div>
+                <div class="row">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4>Warranty Information</h4>
+                        </div>
+                        <div class="panel-body">
+                            <ul>
+                                <li>
+                                    Pride AC and Heating will warranty all labor for a period of one year.
+                                </li>
+                                <li>
+                                    Every Trane residential unit comes with a Base Limited Warranty, which covers parts
+                                    that break because of manufacturer defects.
+                                    The Base Limited Warranty can range from 1 year to 20 years depending on the product.
+                                </li>
+                                <li>
+                                    If you register your product within 60 days of installation, either by phone or online,
+                                    then it becomes a Registered Limited Warranty at no additional cost.
+                                </li>
+                                <li>
+                                    The Registered Limited Warranty can range from 5 years to lifetime depending on the product.
+                                    Registration can be completed online or by phone.
+                                </li>
+                                <li>
+                                    <a href="https://www.trane.com/residential/en/for-owners/warranty-and-registration.html">You can register your Trane Product through this link.</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
