d27fc0444b06df4998c780386e689f7958fea38c
stephenhartman <stephen.c.hartman@gmail.com>
2018-01-22 10:26:30 -0500

    Add mail layouts published, add background css, update app name in config

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Pride AC Jacksonville'),
+    'name' => 'Pride AC Jacksonville',
 
     /*
     |--------------------------------------------------------------------------
--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,11 +1,11 @@
 @component('mail::message')
         ## You received a message from {{ $name }}.
 
-        Name: {{ $name }}
-        Email: {{ $email }}
-        Phone Number: {{ $phone }}
-        Square Footage of Home: {{ $sq_footage }}
-        Additional Notes: {{ $notes }}
+        -Name: {{ $name }}
+        -Email: {{ $email }}
+        -Phone Number: {{ $phone }}
+        -Square Footage of Home: {{ $sq_footage }}
+        -Additional Notes: {{ $notes }}
 
         Thanks,
         {{ config('app.name') }}
