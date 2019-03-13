b4ce2dc3af8dd953b8a634ca4050c38654ec66bd
stephenhartman <stephen.c.hartman@gmail.com>
2019-03-12 20:12:50 -0400

    Add progressive web app support

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,6 +27,18 @@
 </div>
 </body>
 <!-- Scripts -->
-<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ asset('js/app.js') }}">
+if ('serviceWorker' in navigator ) {
+            window.addEventListener('load', function() {
+                navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+                    // Registration was successful
+                    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+                }, function(err) {
+                    // registration failed :(
+                    console.log('ServiceWorker registration failed: ', err);
+                });
+            });
+        }
+</script>
 @stack('scripts')
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,39 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+mix.webpackConfig({
+    plugins: [
+    new SWPrecacheWebpackPlugin({
+        cacheId: 'pwa',
+        filename: 'service-worker.js',
+        staticFileGlobs: ['public/**/*.{css,eot,svg,ttf,woff,woff2,js,html}'],
+        minify: true,
+        stripPrefix: 'public/',
+        handleFetch: true,
+        dynamicUrlToDependencies: { //you should add the path to your blade files here so they can be cached
+               //and have full support for offline first (example below)
+            '/': ['resources/views/home.blade.php'],
+            '/about': ['resources/views/about/index.blade.php'],
+            '/contact': ['resources/views/contact/create.blade.php'],
+            '/contact/index': ['resources/views/contact/index.blade.php'],
+            '/login': ['resources/views/auth/login.blade.php'],
+            '/register':['resources/views/auth/register.blade.php'],
+            '/products':['resources/views/products/index.blade.php'],
+            '/american':['resources/views/products/american/index.blade.php'],
+            '/trane':['resources/views/products/trane/index.blade.php'],
+            '/goodman':['resources/views/products/goodman/index.blade.php'],
+        },
+        staticFileGlobsIgnorePatterns: [/\.map$/, /mix-manifest\.json$/, /manifest\.json$/, /service-worker\.js$/],
+        navigateFallback: '/',
+        runtimeCaching: [
+            {
+                urlPattern: /^https:\/\/fonts\.googleapis\.com\//,
+                handler: 'cacheFirst'
+            }
+        ],
+        // importScripts: ['./js/push_message.js']
+    })
+    ]
+});
