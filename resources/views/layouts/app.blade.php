026f0a1102a72c0aa15c0ddd0d213cee7115d08a
stephenhartman <stephen.c.hartman@gmail.com>
2019-03-12 20:19:57 -0400

    Fix typo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,12 @@
     <!-- Microdata markup added by Google Structured Data Markup Helper. -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">p
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Heating and Air Conditioning Service and Installation Company serving Northeast Florida and the Greater Jacksonville Area">
     <meta name="keywords" content="Air Conditioning, Heating, HVAC, Jacksonville, Northeast Florida, Heating and Air, AC Service, AC Installation">
     <meta name="theme-color" content="#0a75d2"/>
     <meta name="robots" content="index,follow">
     <title>@yield('title', 'Pride AC Jacksonville')</title>
-    @yield('meta')
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
