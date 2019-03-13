3e1fbec7f0989592293a1f23567cbe6b872ed3d5
stephenhartman <stephen.c.hartman@gmail.com>
2019-03-12 20:18:32 -0400

    Add meta to layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,5 @@
 @extends('layouts.app')
 
-@section('meta')
-    <meta name="description" content="Heating and Air Conditioning Service and Installation Company serving Northeast Florida and the Greater Jacksonville Area">
-    <meta name="keywords" content="Air Conditioning, Heating, HVAC, Jacksonville, Northeast Florida, Heating and Air, AC Service, AC Installation">
-    <meta name="robots" content="index,follow">
-@endsection
-
 @push('scripts')
     <script>
         $(document).ready(function() {
